c23a495e63273d75b4b51baa6fb46285c88680a4
552020 <stefanolombardo@posteo.de>
2025-02-09 21:29:34 +0100

    chore: add missing utils from demo blockchain with minor variations

--- src/frontend/src/utils/node.ts
+++ src/frontend/src/utils/node.ts
@@ -1,7 +1,11 @@
 import {
   getApplicationIdFromLocalStorage,
   getNodeUrlFromLocalStorage,
-} from "./storage";
+  getContextIdFromLocalStorage,
+  setContextIdToLocalStorage,
+  getJWTObject,
+  setApplicationIdToLocalStorage,
+} from './storage';
 
 export const getNodeUrl = (): string => {
   // Get from environment variables
@@ -10,7 +14,7 @@ export const getNodeUrl = (): string => {
   if (!nodeUrl) {
     const nodeUrlFromEnv = import.meta.env.VITE_NODE_URL;
     console.warn(
-      "Node URL not found in localStorage, using environment variable",
+      'Node URL not found in localStorage, using environment variable',
     );
     return nodeUrlFromEnv;
   }
@@ -18,11 +22,42 @@ export const getNodeUrl = (): string => {
   return nodeUrl;
 };
 
+export function getContextId(): string {
+  const storageContextId = getContextIdFromLocalStorage();
+
+  if (!storageContextId) {
+    const jwtToken = getJWTObject();
+    const envKey: string = jwtToken?.context_id ?? '';
+    setContextIdToLocalStorage(envKey);
+    return envKey;
+  }
+
+  return storageContextId ?? '';
+}
+
 export const getApplicationId = (): string | null => {
   const applicationId = getApplicationIdFromLocalStorage();
   if (!applicationId) {
     const applicationIdFromEnv = import.meta.env.VITE_APPLICATION_ID;
+    setApplicationIdToLocalStorage(applicationIdFromEnv);
     return applicationIdFromEnv;
   }
   return applicationId;
 };
+
+export function getNearEnvironment(): string {
+  return import.meta.env['VITE_NEAR_ENVIRONMENT'] ?? 'testnet';
+}
+
+// Name has been changed between getApplicationId and getStorageApplicationId
+// export function getStorageApplicationId(): string {
+//   const storageApplicationId = getApplicationIdFromLocalStorage();
+
+//   if (!storageApplicationId) {
+//     const envKey: string = import.meta.env['VITE_APPLICATION_ID'] ?? '';
+//     setApplicationIdToLocalStorage(envKey);
+//     return envKey;
+//   }
+
+//   return storageApplicationId ?? '';
+// }
